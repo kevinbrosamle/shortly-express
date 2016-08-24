ee61281b6dbaed6967447ec5c263b3a3222becc6
kevinbrosamle <kevin.brosamle@gmail.com>
2016-08-23 17:07:25 -0700

    implement hashing of passwords on account creation

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,29 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+  initialize: function() {
+    this.on('creating', this.hashPassword, this);
+  },
+  hashPassword: function(model, attrs, options) {
+    return new Promise(function(resolve, reject) {
+      var salt = bcrypt.genSaltSync(10);
+      // console.log(salt, 'salt');
+      bcrypt.hash(model.attributes.password, salt, null, function(err, hash) {
+        if (err) { 
+          console.log(err); 
+        }
+        model.set('password', hash);
+        console.log(model.get('password'), 'hashed password');
+        resolve(hash);
+      });
+    });
+  }
+  // getPassword: function() {
+  //   var test = this.get('password');
+  //   console.log(test, '<<<<< is the password gotten!');
+  // }
 });
+
 
 module.exports = User;
